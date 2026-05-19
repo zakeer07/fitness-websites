a117bde76b3ea216abf0096aebef6b97b4bb91ca
zakeer07 <zakeer07@users.noreply.github.com>
2026-05-19 18:34:49 +0000

    deploy(production): 8dd2daae8603e44ce89ec27919ebd1c4874be3ee 8dd2daae8603e44ce89ec27919ebd1c4874be3ee

--- shared/auth-config.js
+++ shared/auth-config.js
@@ -1,4 +1,4 @@
 window.FITNESS_AUTH_CONFIG = {
-  supabaseUrl: "https://rhxrvlmpxuiimpbca.supabase.co",
+  supabaseUrl: "https://rhxrvlmpxuiimpbcaruk.supabase.co",
   supabaseAnonKey: "sb_publishable_CFRggzJ4nMXnNiwjnxu1Ig_FATzBsQv",
 };
